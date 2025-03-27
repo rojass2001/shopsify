f27c847697a8e4d49b35b1f66860980c46c39b5f
rojass2001 <rojascherian@gmail.com>
2025-03-27 23:11:26 +0530

    third commit

--- src/Components/Carttotal.jsx
+++ src/Components/Carttotal.jsx
@@ -7,8 +7,8 @@ function Cartsubtotal() {
   <div className='w-full relative space-y-2 min-h-[200px]  text-blue-700 font-bold border-[4px]'>
     <div className='flex justify-between bg-blue-700 text-white w-full py-2' >Total Items:<span>{cartproducts.length}</span>
      </div>
-     <div className='flex w-full text-blue-700 p-2 font-extrabold justify-between'>Subtotal:<span>${totalprice}</span></div>
-     <div className='flex w-full p-2 text-blue-700 font-extrabold justify-between  '>TotalPrice:<span>${totalprice}</span></div>
+     <p className='flex w-full text-blue-700 p-2 font-extrabold justify-between'>Subtotal:<span>${totalprice}</span></p>
+     <p className='flex w-full p-2 text-blue-700 font-extrabold justify-between  '>TotalPrice:<span>${totalprice}</span></p>
     <div className='w-full px-1 flex justify-center  absolute bottom-1 py-2 gap-2 text-white '>
       <button className='w-[120px] h-8 bg-blue-700'>Buynow</button>
       <Link to="/products"><button  className='w-[120px] h-8  bg-blue-700'>Continue Shop</button></Link>
--- src/Pages/Productdetail.jsx
+++ src/Pages/Productdetail.jsx
@@ -9,27 +9,23 @@ const dispatch=useDispatch()
 const {productdetails}=useSelector(state=>state.product)
   return (
    
-    <div className='w-full min-h-screen pt-[130px] place-items-center place-content-center p-2 '>
-
-        <div className='w-full   md:w-[70%] lg:h-[500px] shadow-lg flex shadow-gray-300 flex-col md:flex-row gap-2 p-5 '>
-        <img className='w-full h-[200px] md:w-[50%] md:h-full' src={productdetails?.image}/>
+ <div className='w-full min-h-screen pt-[130px] place-items-center place-content-center p-2 '>
+  <div className='w-full   md:w-[70%] lg:h-[500px] shadow-lg flex shadow-gray-300 flex-col md:flex-row gap-2 p-5 '>
+      <img className='w-full h-[200px] md:w-[50%] md:h-full' src={productdetails?.image}/>
     <div className='space-y-2 font-bold text-center place-items-center md:text-start md:place-items-start'>
-        <p className='text-2xl'>{productdetails?.title}</p>
-        <p className='text-2xl'>Price:${Math.floor(productdetails?.price)}</p>
-        <p>{productdetails?.description}</p>
-      <div className='w-full flex justify-center md:justify-start items-center gap-1'> <FaStar/>{productdetails?.rating?.rate}</div>
-      <div className='w-full justify-center md:justify-start items-center flex text-black gap-2  '>
-        <button onClick={()=>dispatch(decreaseproductquantity())} className='bg-black w-10 h-7 text-white'>-</button>{productdetails.quantity}
-        <button  onClick={()=>dispatch(increaseproductquantity())} className='bg-black w-10 h-7 text-white'>+</button>
-      </div>
+      <p className='text-2xl'>{productdetails?.title}</p>
+      <p className='text-2xl'>Price:${Math.floor(productdetails?.price)}</p>
+      <p>{productdetails?.description}</p>
+      <p className='text-black flex items-center'><FaStar/> {productdetails?.rating?.rate}</p>
+      <button onClick={()=>dispatch(decreaseproductquantity())} className='bg-black w-10 h-7 text-white mr-2'>-</button>{productdetails.quantity}
+      <button  onClick={()=>dispatch(increaseproductquantity())} className='bg-black w-10 h-7 ml-2 text-white'>+</button>
       <p className='text-xl'>subtotal:${productdetails?.subtotal}</p>
-      <div className='w-full justify-center md:justify-start flex gap-3 mt-5 text-white'>
-        <button className='bg-black w-[100px] h-9'>BuyNow</button>
-        <button onClick={()=>dispatch(addtocart(productdetails))} className='bg-black w-[100px] h-9'>Addtocart</button>
-      </div>
+      <button className='bg-black w-[100px] h-10 text-white'>Buy now</button>
+      <button onClick={()=>dispatch(addtocart(productdetails))}
+      className='bg-black w-[100px] h-10 text-white ml-5'>Addtocart</button>   
    </div>
-        </div>
-    </div>
+  </div>
+ </div>
  
   )
 }
