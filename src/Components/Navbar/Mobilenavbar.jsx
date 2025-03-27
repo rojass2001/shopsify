381637f1bf27593725d922e6ccb5a9567a953b81
rojass2001 <rojascherian@gmail.com>
2025-03-27 22:14:05 +0530

    third commit

--- src/Components/Navbar/Mobilenavbar.jsx
+++ src/Components/Navbar/Mobilenavbar.jsx
@@ -8,7 +8,7 @@ const mobilenav=[{name:"Home",link:"/"},
     {name:"signup",link:"/login"}]
 function Mobilenavbar({navbarpopup}) {
   return (
-    <div className='bg-blue-800 pt-10 place-items-center absolute h-screen top-0 left-0 w-[92%]'>
+    <div className='bg-blue-800 pt-10 text-center absolute h-screen top-0 left-0 w-[92%]'>
        <div className='text-white text-2xl  font-bold'>
         {mobilenav.map((a)=>(
   <Link to={a?.link} key={a}> <p onClick={navbarpopup} className='mb-7' >{a.name}</p></Link>
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 function Login() {
   return (
-    <div className='w-full px-2 pt-[100px] min-h-screen flex items-center place-content-center'>
+    <div className='w-full px-2 pt-[115px] min-h-screen flex items-center place-content-center'>
       <div className='w-full pb-8 md:pb-0 md:w-[60%] shadow-md h-auto md:h-[400px] flex flex-col md:flex-row  gap-5 bg-white shadow-gray-300'>
   <div className='w-full md:w-[50%] py-4 rounded-b-[20%] md:rounded-b-[0%] text-white flex flex-col gap-5 bg-blue-600  place-content-center place-items-center h-full'>
     <h2 className='font-bold text-4xl'>Hello, welcome</h2>
