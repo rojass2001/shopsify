46d076c4f8413b1142215ae8e163687255645be3
rojass2001 <rojascherian@gmail.com>
2025-03-27 22:21:29 +0530

    third commit

--- src/Components/Carttotal.jsx
+++ src/Components/Carttotal.jsx
@@ -5,14 +5,13 @@ function Cartsubtotal() {
   const {totalprice,cartproducts}=useSelector(state=>state.cart)
   return (
   <div className='w-full relative space-y-2 min-h-[200px]  text-blue-700 font-bold border-[4px]'>
-    <div className='flex justify-between text-white w-full py-2
-    ' style={{backgroundColor:"blue"}}>Total Items:<span>{cartproducts.length}</span>
+    <div className='flex justify-between bg-blue-700 text-white w-full py-2' >Total Items:<span>{cartproducts.length}</span>
      </div>
-     <div className='flex w-full text-black p-2 font-extrabold justify-between'>Subtotal:<span>${totalprice}</span></div>
-     <div className='flex w-full p-2 text-black font-extrabold justify-between  '>TotalPrice:<span>${totalprice}</span></div>
+     <div className='flex w-full text-blue-700 p-2 font-extrabold justify-between'>Subtotal:<span>${totalprice}</span></div>
+     <div className='flex w-full p-2 text-blue-700 font-extrabold justify-between  '>TotalPrice:<span>${totalprice}</span></div>
     <div className='w-full px-1 flex justify-center  absolute bottom-1 py-2 gap-2 text-white '>
-      <button className='w-[120px] h-8 bg-black'>Buynow</button>
-      <Link to="/products"><button  className='w-[120px] h-8  bg-black'>Continue Shop</button></Link>
+      <button className='w-[120px] h-8 bg-blue-700'>Buynow</button>
+      <Link to="/products"><button  className='w-[120px] h-8  bg-blue-700'>Continue Shop</button></Link>
     </div>
   </div>
   )
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -2,19 +2,20 @@ import { FaStar, FaTrash } from 'react-icons/fa'
 import { useDispatch, useSelector } from 'react-redux'
 import Carttotal from '../Components/Carttotal'
 import { cartdecrease, cartincrease, removecart } from '../redux/Cartslice'
-const quantitybutton ="bg-black text-white w-9 h-6"
+const quantitybutton ="bg-blue-700 text-white w-9 h-6"
   function Cart() {
   const dispatch=useDispatch()
   const {cartproducts}=useSelector(state=>state.cart)
   return (
     <div className='w-full pt-[120px] md:pt-[60px] ' >
-      <div className='text-3xl py-4 bg-blue-600 font-bold text-white mb-5 text-center'>Your Carts</div> 
+      <div className='text-3xl py-4 bg-blue-700 font-bold text-white mb-5 text-center'>Your Carts</div> 
       {cartproducts.length<1?
       <div className='w-full h-screen text-center text-blue-700 text-3xl font-bold' >Your Cart is Empty</div>
-     :<div className='w-full min-h-screen px-2 flex flex-col-reverse md:flex-row gap-3 justify-around'>
+    
+    :<div className='w-full min-h-screen px-2 flex flex-col-reverse md:flex-row gap-3 justify-around'>
         <div className='w-full md:w-[60%]'>
       {cartproducts?.map((a)=>(
-      <div className='w-full mb-5 py-2 h-[280px] md:h-[250px] gap-2 text-black shadow-md shadow-gray-300 flex' key={a.id}>
+      <div className='w-full mb-5 py-2 h-[280px] md:h-[250px] gap-2 text-blue-700 shadow-md shadow-gray-300 flex' key={a.id}>
         <img className='w-[40%] h-full' src={a.image} />
       <div className='font-bold relative space-y-2'>
           <p className='text-2xl'>{a.title.slice(0,38)}</p>
@@ -24,6 +25,7 @@ const quantitybutton ="bg-black text-white w-9 h-6"
            <button  onClick={()=>dispatch(cartdecrease(a.id))} className={quantitybutton}>-</button>{a.quantity}
            <button  onClick={()=>dispatch(cartincrease(a.id))} className={quantitybutton}>+</button>
           </div>
+
         <p>subtotal:${a.subtotal}</p>
         <div onClick={()=>dispatch(removecart(a))} className='w-14  place-items-center place-content-center 
           h-7 rounded-full text-xl bg-white shadow-mdshadow-gray-300'><FaTrash/>
@@ -31,6 +33,7 @@ const quantitybutton ="bg-black text-white w-9 h-6"
       </div>
       </div>
     ))  }
+
     </div>
       <div className='w-full md:w-[30%] lg:w-[20%]'>
        <Carttotal/>
