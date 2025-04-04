2bd524057933ce7065f02b550666a46930726495
rojass2001 <rojascherian@gmail.com>
2025-04-04 15:19:55 +0530

    third commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ function About() {
   return (
     <div className="w-full mt-[120px] md:mt-[60px] ">
     <div className="h-screen relative">
-        <Image className='w-full h-full' fill  src="/about.jpg" alt="image loading" />
+        <Image className='w-full h-full' fill unoptimized  src="/about.jpg" alt="image loading" />
     </div>
     </div>
   )
