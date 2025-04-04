07e025df9f6085c874ff18921175688f4af74545
rojass2001 <rojascherian@gmail.com>
2025-04-04 15:28:47 +0530

    third commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import img from '../../../public/images/about.jpg'
 function About() {
   return (
     <div className="w-full mt-[120px] md:mt-[60px] ">
     <div className="h-screen relative">
-        <Image className='w-full h-full' fill unoptimized  src="/about.jpg" alt="image loading" />
+        <Image className='w-full h-full' fill  src={img} alt="image loading" />
     </div>
     </div>
   )
